1d3890a0081bdac88bf91f5ae03d8215aaf7aa2c
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-18 23:53:07 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ListItems from './ListItems';
+import React from "react";
+import ListItems from "./ListItems";
 
- const List = ({listx}) =>{
-  return(
-    // code here
-    
-      <ListItems  />
-    
-    
-  )
-}
+const List = ({ listx }) => {
+    console.log("listx", listx);
+    return (
+        // code here
+        <>
+            {listx.map((item) => (
+                <ListItems key={item} valuex={item} />
+            ))}
+        </>
+    );
+};
 
 export default List;
